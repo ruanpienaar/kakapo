c45df9325c701a13e87d640bce70b4063efbc2cb
ruanpienaar <ruan800@gmail.com>
2017-02-24 08:40:49 +0000

    @

--- src/kakapo_app.erl
+++ src/kakapo_app.erl
@@ -17,29 +17,28 @@ start() ->
     application:start(kakapo).
 
 start(_StartType, _StartArgs) ->
+    Handlers = application:get_env(kakapo, event_handler, [kakapo]),
     %% Just play around with error_logger
     % ok = error_logger:add_report_handler(kakapo,{}),
     LogDir = application:get_env(kakapo, log_dir, "log/"),
     LogFileSize = application:get_env(kakapo, log_file_size, 1 * 1024 * 1024),
     LogFileMax = application:get_env(kakapo, log_file_max, 10),
-
-    Handlers = application:get_env(kakapo, event_handler, [kakapo]),
-    start_error_logger_handlers(Handlers),
-
+    ok = start_error_logger_handlers(Handlers, LogDir, LogFileSize, LogFileMax),
     kakapo_sup:start_link().
 
 stop(_State) ->
     ok.
-start_error_logger_handlers() ->
+
+start_error_logger_handlers([], LogDir, LogFileSize, LogFileMax) ->
     ok;
-start_error_logger_handlers([H|T]) ->
+start_error_logger_handlers([H|T], LogDir, LogFileSize, LogFileMax) ->
     case H of
         log_mf_h ->
             % 5 Mb each, 100 total files
             Args = log_mf_h:init(LogDir, LogFileSize, LogFileMax),
             ok = error_logger:add_report_handler(log_mf_h, Args),
-            start_error_logger_handlers(T);
+            start_error_logger_handlers(T, LogDir, LogFileSize, LogFileMax);
         kakapo_h ->
             ok = error_logger:add_report_handler(kakapo_h,[LogDir, LogFileSize, LogFileMax]),
-            start_error_logger_handlers(T)
+            start_error_logger_handlers(T, LogDir, LogFileSize, LogFileMax)
     end.
